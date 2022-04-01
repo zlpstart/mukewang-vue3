35b1293d1cacae2122653e11e25940f876cc229b
zlp <844023644@qq.com>
2022-04-01 11:26:56 +0800

    feat(git): add commitizen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -11,8 +11,12 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+
   components: {
     HelloWorld
+  },
+  created() {
+    console.log('呵呵')
   }
 }
 </script>
