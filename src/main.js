6c9d6dcefdf3316765facb9cb68d4ac47bc9fb30
zlp <844023644@qq.com>
2022-04-02 16:23:40 +0800

    feat: 封装图标

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '@/style/index.scss'
+// 导入 svgIcon
+import installIcons from '@/icons/index'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installIcons(app)
+
+app.use(ElementPlus).use(store).use(router).mount('#app')
